c52fed8927716aac9510c400fb21499a81024a91
Narmalone <62171923+Narmalone@users.noreply.github.com>
2023-03-14 19:32:21 +0100

    build

--- LauncherAppTest/RunnerGameLauncher/obj/Debug/net6.0-windows/App.g.cs
+++ LauncherAppTest/RunnerGameLauncher/obj/Debug/net6.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace RunnerGameLauncher {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace RunnerGameLauncher {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
         public static void Main() {
             RunnerGameLauncher.App app = new RunnerGameLauncher.App();
             app.InitializeComponent();
